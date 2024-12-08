fa7b10df9e42be308260cc1e2789f379ff8a24eb
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2024-12-08 16:28:40 +0530

    fix frontend

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,60 +1,21 @@
-import { useSearchParams } from 'react-router-dom';
-import axios from "axios";
-import { useState } from 'react';
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SubHeading } from "../components/SubHeading"
 
-export const SendMoney = () => {
-  const [searchParams] = useSearchParams();
-  const id = searchParams.get("id");
-  const name = searchParams.get("name");
-  const [amount, setAmount] = useState(0);
-
-  return <div class="flex justify-center h-screen bg-gray-100">
-    <div className="h-full flex flex-col justify-center">
-      <div
-        class="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg"
-      >
-        <div class="flex flex-col space-y-1.5 p-6">
-          <h2 class="text-3xl font-bold text-center">Send Money</h2>
+export const Signin = () => {
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <Heading label={"Sign in"} />
+        <SubHeading label={"Enter your credentials to access your account"} />
+        <InputBox placeholder="rahul@gmail.com" label={"Email"} />
+        <InputBox placeholder="123456" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign in"} />
         </div>
-        <div class="p-6">
-          <div class="flex items-center space-x-4">
-            <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-              <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
-            </div>
-            <h3 class="text-2xl font-semibold">{name}</h3>
-          </div>
-          <div class="space-y-4">
-            <div class="space-y-2">
-              <label
-                class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                for="amount"
-              >
-                Amount (in Rs)
-              </label>
-              <input
-                onChange={(e) => {
-                  setAmount(e.target.value);
-                }}
-                type="number"
-                class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
-                id="amount"
-                placeholder="Enter amount"
-              />
-            </div>
-            <button onClick={() => {
-              axios.post("http://localhost:3000/api/v1/account/transfer", {
-                to: id,
-                amount
-              }, {
-                headers: {
-                  Authorization: "Bearer " + localStorage.getItem("token")
-                }
-              })
-            }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
-              Initiate Transfer
-            </button>
-          </div>
-        </div>
+        <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
       </div>
     </div>
   </div>
