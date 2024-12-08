2694d22f1347c854dd47dabbd292a97fd5704f43
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2024-12-08 13:44:19 +0530

    set user routes

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", rootRouter);
+app.use("/api/v1/user", userRouter);
+
+app.listen(3000);
