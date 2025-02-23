006c5b16a260bcbcabac8883fcba6d12585328e6
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2025-02-23 22:52:20 +0530

    change localhost to backendurl

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,6 +6,7 @@ import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 import axios from "axios";
 import { useNavigate } from "react-router-dom"
+import { BACKEND_URL } from "../config"
 
 export const Signup = () => {
   const [firstName, setFirstName] = useState("");
@@ -33,7 +34,7 @@ export const Signup = () => {
         }} placeholder="123456" label={"Password"} />
         <div className="pt-4">
           <Button onClick={async () => {
-            const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
               username,
               firstName,
               lastName,
